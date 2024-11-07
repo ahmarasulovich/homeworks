fc338815c5731c8de3a623a062834aef761bbd21
ahmarasulovich <ahmadoneplus7t@gmail.com>
2024-11-07 22:28:01 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+#1st program
+print((9**0.5)*5)
+
+#2nd program
+print(9.99 > 9.98, 1000!=1000.1)
+
+#3rd program
+print(2*2+2 == 2*(2+2))
+
+#4th program
+    #Вариант №1
+a= (float('123.456') *10)
+print(int(a/308.5))
+
+    #Вариант №2
+b= (int(a))
+print(b//10**2//3)
+
+    #Вариант №3
+print(b%10)
